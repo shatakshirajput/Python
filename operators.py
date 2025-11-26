3e0ac431c8a87e1ae73aaae2e6f7b09c87f28e56
Shatakshi Rajput <shatakshirajput02@gmail.com>
2025-11-26 21:48:47 +0530

    python

---
+++ operators.py
@@ -0,0 +1,45 @@
+# operators
+''' 1. Arithmetic 
+    2. Relational
+    3. Assignment
+    4. Logical
+'''
+
+# arithmetic
+a = 45
+b = 34
+print(a+b)
+print(a-b)
+print(a/b)
+print(a%b)
+print(a*b)
+print(a**b)
+print(a//b)
+
+# relational
+print(a>b)
+print(a>=b)
+print(a<b)
+print(a<=b)
+print(a!=b)
+print(a==b)
+
+# assignment
+c = a
+print(c)
+a += 1 
+print(a)
+a -= 1
+print(a)
+a *= 6
+print(a)
+a /= 1
+print(a)
+
+# logical
+var = False
+var1 = True
+print(not var)
+print(not(a>b))
+print(var and var1)
+print(var or var1)
---
+++ type_conversion.py
@@ -0,0 +1,7 @@
+# type conversion and casting
+
+a = 10.5 + 5           # conversion
+b = int(10.5 + 5)      # casting
+
+print(a)
+print(b)
