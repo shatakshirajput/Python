e2aa2bb56f83557dae18d969a1ad7713987c7fb7
Shatakshi Rajput <shatakshirajput02@gmail.com>
2025-12-01 09:55:40 +0530

    python

---
+++ Functions/avg_sum.py
@@ -0,0 +1,9 @@
+def avg(a,b,c):
+    a = (a+b+c)/3
+    return a
+
+n1 = int(input("Enter the first number : "))
+n2 = int(input("Enter the second number : "))
+n3 = int(input("Enter the third number : "))
+
+print(avg(n1,n2,n3))
---
+++ Functions/factorial.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    f = 1
+    for i in range(1,n+1):
+        f = f * i
+    return f
+
+n = int(input("Enter the number : "))
+print(factorial(n))
