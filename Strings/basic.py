9f10be7abc54a29df7102d0ddcc649a60a3edca4
Shatakshi Rajput <shatakshirajput02@gmail.com>
2025-12-04 21:35:27 +0530

    python

--- Strings/basic.py
+++ Strings/basic.py
@@ -2,6 +2,6 @@ str = "python"
 
 print(len(str))
 
-# slicing str[start : end : step] ; default start = 0; end = length of string; step = 1
+# slicing str[start : end : step] default start = 0, end = length of string, step = 1
 print(str[1:4])
 print(str[1:5:2])
